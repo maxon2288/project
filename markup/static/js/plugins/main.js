53741f2a1311a495696903397f1965171dba8332
maxon2288 <maxswim228@gmail.com>
2019-08-12 18:59:59 +0600

    :gfdfdssdf

--- markup/static/js/plugins/main.js
+++ markup/static/js/plugins/main.js
@@ -1,23 +1,95 @@
 
 $(document).ready(function () {
-	// $("body").css({'visibility': "visible", "opacity": "1"});
 	popup ();
-	// forms();
 
-	var swiper = new Swiper('.slider', {
-		spaceBetween: 10,
-		navigation: {
-			nextEl: '.slider-next',
-			prevEl: '.slider-prev',
+	$("body").css({"visibility": "visible", "opacity": "1"})
+
+	var swiper = new Swiper('.made-slider', {
+		slidesPerView: 3,
+		loop: true,
+		autoplay: {
+			delay: 3000,
 		},
 	});
 
+	var swiper = new Swiper('.team-slider', {
+		slidesPerView: 6,
+		spaceBetween: 20,
+		loop: true,
+		autoplay: {
+			delay: 2500,
+		},
+	});
+	var a = 0;	
 	var lazyLoadInstance = new LazyLoad({
 		elements_selector: "img"
 		// ... more custom settings?
 	});
 
+	if ($(".team").length > 0) {
+		var oTop = $('.team').offset().top - window.innerHeight +150;
+		if (a == 0 && $(window).scrollTop() > oTop) {
+			$('.counter-value').each(function() {
+			var $this = $(this),
+				countTo = $this.attr('data-count');
+				var floor = Math.floor(Math.random() * 20) + 20;
+				var floor2 = floor * 100;
+				$({
+					countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				},
+		
+				{
+					duration: floor2,
+					easing: 'swing',
+					step: function() {
+						$this.text(Math.floor(this.countNum));
+					},
+					complete: function() {
+						$this.text(this.countNum);
+						//alert('finished');
+				}
+				});
+			});
+			a = 1;
+		}	
+		
+		$(window).scroll(function() {
+			
+			var oTop = $('.team').offset().top - window.innerHeight +150;
+			if (a == 0 && $(window).scrollTop() > oTop) {
+				$('.counter-value').each(function() {
+					var $this = $(this),
+					countTo = $this.attr('data-count');
+					var floor = Math.floor(Math.random() * 20) + 20;
+		var floor2 = floor * 100;
+		console.log(floor2);
+					$({
+				countNum: $this.text()
+			}).animate({
+				countNum: countTo
+				},
 	
+				{
+	
+				duration: floor2,
+				easing: 'swing',
+				step: function() {
+					$this.text(Math.floor(this.countNum));
+				},
+				complete: function() {
+					$this.text(this.countNum);
+					//alert('finished');
+				}
+	
+				});
+			});
+			a = 1;
+		}
+	
+		});
+	}
 
 
 	$(document).ready(function() {
